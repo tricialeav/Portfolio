2f7e910ede0189a4bd22b6ef63a62195152e083e
Tricia Leavitt <leavitt.tricia@gmail.com>
2019-04-18 22:09:18 -0700

    Mobile optimizations

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -10,11 +10,25 @@ export default class About extends Component {
 
         this.state = {
             type: 'all',
-            animation: ''
+            animation: '',
+            isDesktop: false
         }
 
         this.filterSkills = this.filterSkills.bind(this);
         this.moveArrow = this.moveArrow.bind(this);
+        this.updatePredicate = this.updatePredicate.bind(this);
+    }
+    componentDidMount() {
+        this.updatePredicate();
+        window.addEventListener("resize", this.updatePredicate);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updatePredicate);
+    }
+
+    updatePredicate() {
+        this.setState({ isDesktop: window.innerWidth > 1200 });
     }
 
     filterSkills = (e) => {
@@ -48,37 +62,53 @@ export default class About extends Component {
                             <div id='aboutText'>
                                 <p>I spent the first ten years of my career in the business world, where I sharpened my interpersonal communication, relationship building, and leadership skills.</p>
                                 <p>After transitioning my career in early 2018, I have worked as a Software Developer and Freelancer.</p>
-                                <p>I am also the Cofounder of <a href='https://www.meetup.com/Whiteboard-Warriors/' className='inlineLinks' target='_blank' rel="noopener">Whiteboard Warriors Meetup Group</a>, located in Irvine, CA. Our purpose is to provide hands-on technical interview practice to developers of all skill levels in a fun and collaborative environment.</p>
+                                <p>I am also the Cofounder of <a href='https://www.meetup.com/Whiteboard-Warriors/' className='inlineLinks' target='_blank' rel="noopener noreferrer">Whiteboard Warriors Meetup Group</a>, located in Irvine, CA. Our purpose is to provide hands-on technical interview practice to developers of all skill levels in a fun and collaborative environment.</p>
                                 <p>In my free time, you will find me gardening, working on personal development projects, promoting the Meetup Group, taking pictures, and enjoying beautiful Southern California.</p>
                             </div>
                         </div>
                     </div>
-                    <div className='aboutColumns'>
-                        <h1>Skills <small>(hover)</small></h1>
-                        <div className='cardRow'>
-                            {skills.map(({ icon, skill, years, type }) =>
-                                (
-                                    type === this.state.type || this.state.type === "all" ? (
-                                        <div className='flipCard' value={type} key={skill}>
-                                            <div className='flipCardInner' >
-                                                <div >
-                                                    <div className='flipCardFront' >
-                                                        <i className={icon} ></i>
-                                                        <h5>{skill}</h5>
-                                                    </div>
-                                                    <div className='flipCardBack'>
-                                                        <h5>{years}</h5>
+                    {this.state.isDesktop ?
+                        <div className='aboutColumns'>
+                            <h1>Skills <small>(hover)</small></h1>
+                            <div className='cardRow'>
+                                {skills.map(({ icon, skill, years, type }) =>
+                                    (
+                                        type === this.state.type || this.state.type === "all" ? (
+                                            <div className='flipCard' value={type} key={skill}>
+                                                <div className='flipCardInner' >
+                                                    <div >
+                                                        <div className='flipCardFront' >
+                                                            <i className={icon} ></i>
+                                                            <h5>{skill}</h5>
+                                                        </div>
+                                                        <div className='flipCardBack'>
+                                                            <h5>{years}</h5>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </div>) : (
-                                            null
-                                        )
+                                            </div>) : (
+                                                null
+                                            )
+                                    )
+                                )}
+                            </div>
+                        </div>
+                        :
+                        <div className='mobileSkills'>
+                            <hr />
+                            <h1>Skills</h1>
+                            {skills.map(({ skill }) =>
+                                (
+                                    <ul>
+                                        <li>{skill}</li>
+                                    </ul>
                                 )
                             )}
+                            <hr/>
                         </div>
-                    </div>
+                    }
                 </div>
+                {this.state.isDesktop ?
                 <Link
                     activeClass="active"
                     to="portfolio"
@@ -97,6 +127,9 @@ export default class About extends Component {
                     </span>
                     <i class="fas fa-arrow-down"></i>
                 </Link>
+                :
+                null 
+                }
             </div>
 
         );
