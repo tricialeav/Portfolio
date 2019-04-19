c5b10445f1d26f2df3f0db4d5763fd703b45ce8b
Tricia Leavitt <leavitt.tricia@gmail.com>
2019-04-18 21:05:31 -0700

    Replaced <ul> with <div> for accessibility

--- src/views/portfolio/portfolio.js
+++ src/views/portfolio/portfolio.js
@@ -17,78 +17,78 @@ export default class Portfolio extends Component {
             <div className="portfolio" id='portfolio'>
                 <div id='portfolioContainer'>
                     <h1>My Work</h1>
-                    <ul id='portfolioList'>
+                    <div id='portfolioList'>
                         <Link to='/uci'>
-                            <li className='portfolioCard'>
+                            <div className='portfolioCard'>
                                 <img src={microphone} alt='Microphone on stand' className='portfolioGraphics'></img>
                                 <div className='portfolioText'>
                                     <h4>UCI Open House</h4>
                                     <p>Speaker</p>
                                     <p>April 2019</p>
                                 </div>
-                            </li>
+                            </div>
                         </Link>
                         {/* <Link to='/uci'> */}
-                            <li className='portfolioCard'>
+                            <div className='portfolioCard'>
                                 <img src={comingSoon} alt='Coming soon placeholder' className='portfolioGraphics'></img>
                                 <div className='portfolioText'>
                                     <h4>Symptom Tracker App</h4>
                                     <p>Personal Project</p>
                                     <p>In Development - Spring 2019</p>
                                 </div>
-                            </li>
+                            </div>
                         {/* </Link>
                         <Link to='/uci'> */}
-                            <li className='portfolioCard'>
+                            <div className='portfolioCard'>
                                 <img src={comingSoon} alt='Coming soon placeholder' className='portfolioGraphics'></img>
                                 <div className='portfolioText'>
                                     <h4>Whiteboard Warriors Website</h4>
                                     <p>Personal Project</p>
                                     <p>In Development - Spring 2019</p>
                                 </div>
-                            </li>
+                            </div>
                         {/* </Link>
                         <Link to='/uci'> */}
-                            <li className='portfolioCard'>
+                            <div className='portfolioCard'>
                                 <img src={code1} alt='Code editor on computer screen' className='portfolioGraphics'></img>
                                 <div className='portfolioText'>
                                     <h4>Subscriber Data Project</h4>
                                     <p>Professional Work</p>
                                     <p>Fall 2018</p>
                                 </div>
-                            </li>
+                            </div>
                         {/* </Link>
                         <Link to='/uci'> */}
-                            <li className='portfolioCard'>
+                            <div className='portfolioCard'>
                                 <img src={code2} alt='Code editor on computer screen' className='portfolioGraphics'></img>
                                 <div className='portfolioText'>
                                     <h4>User Portal Project</h4>
                                     <p>Professional Work</p>
                                     <p>Winter 2018</p>
                                 </div>
-                            </li>
+                            </div>
                         {/* </Link>
                         <Link to='/uci'> */}
-                            <li className='portfolioCard'>
+                            <div className='portfolioCard'>
                                 <img src={magazine} alt='person reading magazine' className='portfolioGraphics'></img>
                                 <div className='portfolioText'>
                                     <h4>Featured Student</h4>
                                     <p>UCI Division of Continuing Education Magazine</p>
                                     <p>Winter 2018</p>
                                 </div>
-                            </li>
+                            </div>
                         {/* </Link>
                         <Link to='/uci'> */}
-                            <li className='portfolioCard'>
+                            <div className='portfolioCard'>
                                 <img src={game} alt='Screenshot of Crystal Collector Game with dragon character' className='portfolioGraphics'></img>
                                 <div className='portfolioText'>
                                     <h4>UCI Final Project</h4>
                                     <p>School Project</p>
                                     <p>Spring 2018</p>
                                 </div>
-                            </li>
+                            </div>
                         {/* </Link> */}
-                    </ul>
+                    </div>
                 </div>
             </div >
         );
